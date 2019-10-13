44f1b843c02d5ea65d1f698a7b9ae59955a93af9
Saranya <sara.29891@gmail.com>
2019-10-12 19:10:01 -0700

    adding files to repo

--- Assets/questions.js
+++ Assets/questions.js
@@ -1,1 +1,21 @@
-
+var questions = [{
+    question: "1. How do you write 'Hello World' in an alert box?",
+    choices: ["msg('Hello World')", "msgBox('Hello World');", "alertBox('Hello World');", "alert('Hello World');"],
+    correctAnswer: 3
+}, {
+    question: "2. How to empty an array in JavaScript?",
+    choices: ["arrayList[]", "arrayList(0)", "arrayList.length=0", "arrayList.len(0)"],
+    correctAnswer: 2
+}, {
+    question: "3. What function to add an element at the begining of an array and one at the end?",
+    choices: ["push,unshift", "unshift,push", "first,push", "unshift,last"],
+    correctAnswer: 1
+}, {
+    question: "4. What will this output? var a = [1, 2, 3]; console.log(a[6]);",
+    choices: ["undefined", "0", "prints nothing", "Syntax error"],
+    correctAnswer: 0
+}, {
+    question: "5. What would following code return? console.log(typeof typeof 1);",
+    choices: ["string", "number", "Syntax error", "undefined"],
+    correctAnswer: 0
+}];
